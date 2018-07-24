7faaf77293d8e60b51cd16bb5c0cebbe7087109e
brandeddavid <david.mathenge98@gmail.com>
2018-07-24 18:44:58 +0300

    Add Hello World component

---
+++ src/components/hello.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Hello extends Component {
+    state = {  }
+    render() { 
+        return <h1 className="container">Hello, World</h1>
+    }
+}
+ 
+export default Hello;
