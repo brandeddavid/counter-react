c6316594411b168d1d7a66adeaa4bcf34bfce089
brandeddavid <david.mathenge98@gmail.com>
2018-07-25 19:16:56 +0300

    Add index page styling

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import "bootstrap/dist/css/bootstrap.css"
 import Counter from "./components/counter"
+import "./static/css/index.css"
 
 ReactDOM.render(<Counter />, document.getElementById('root'));
 registerServiceWorker();
