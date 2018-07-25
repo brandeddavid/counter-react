7726838a7fe93f091dc6c9851b604f45a308c040
brandeddavid <david.mathenge98@gmail.com>
2018-07-25 19:17:40 +0300

    Implement counter on component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,27 +5,35 @@ class Counter extends Component {
     count: 0,
     tags: ["one", "two", "three"]
   };
+
   render() {
     return (
         <div>
             <span className={this.getBadgeClasses()}>{ this.formatCount() }</span>
-            <button className="btn btn-primary btn-sm">Click Me</button>
+            <button onClick={this.handleIncrement} className="btn btn-primary btn-sm">Click Me</button>
             {this.renderTags()}
         </div>
     );
   }
+  handleIncrement = () => {
+      this.setState({count:this.state.count + 1})
+    }
+
   getBadgeClasses = () => {
     let classes = "badge m-2 badge-";
     classes += this.state.count === 0 ? "warning": "primary";
     return classes;
   }
+
   formatCount = () => {
       const { count } = this.state;
       return count === 0 ? "Zero": count;
   }
+
   renderTags = () => {
       return this.state.tags.length === 0 ? <p>There are no tags</p>: <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
   }
+
 }
 
 export default Counter;
