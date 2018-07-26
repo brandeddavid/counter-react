b375ce850eae9a02ee86c88f5458e5597dd61b88
brandeddavid <david.mathenge98@gmail.com>
2018-07-26 12:33:59 +0300

    Add counters component with delete, increment and reset functions

---
+++ src/components/counters.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import Counter from "./counter";
+
+class Counters extends Component {
+    state = { 
+        counters: [
+            {id: 1, value:0 },
+            {id: 2, value:0 },
+            {id: 3, value:0 },
+            {id: 4, value:0 },
+            {id: 5, value:0 }
+        ]
+     }
+    render() { 
+        return (
+            <div>
+                <button className="btn btn-danger btn-sm m-2" onClick={this.handleReset} >Reset</button>
+                {this.state.counters.map(
+                    counter => <Counter 
+                        key={counter.id} 
+                        counter = {counter}
+                        selected={true} 
+                        onDelete={this.handleDelete} 
+                        onAdd={this.handleAdd}
+                        onIncrement={this.handleIncrement} 
+                    />
+                )}
+            </div>
+        );
+    }
+
+    handleAdd = () => {
+        console.log("Add Function Called");
+    }
+
+    handleDelete = (counterId) => {
+        const counters = this.state.counters.filter(counter => counter.id !== counterId)
+        this.setState({counters})
+    }
+
+    handleReset = () => {
+        const counters = this.state.counters.map(counter =>
+            {
+                counter.value = 0;
+                return counter
+            }
+        );
+        this.setState({counters})
+    }
+
+    handleIncrement = (counter) => {
+        const counters = [...this.state.counters]
+        const index = counters.indexOf(counter)
+        counter[index] = {...counter}
+        counter.value++
+        this.setState(counters)
+    }
+}
+ 
+export default Counters;
