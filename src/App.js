a1a7cf7dad4cb4728c957fcf3b22672a9c4f9b01
brandeddavid <david.mathenge98@gmail.com>
2018-07-26 13:32:47 +0300

    Restructure to add NavBar

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Navbar from "./components/navbar"
+import Counters from "./components/counters"
+
+class App extends Component {
+    render() { 
+        return ( 
+            <React.Fragment>
+                <Navbar />
+                <main className="container">
+                    <Counters />
+                </main>
+            </React.Fragment>
+         );
+    }
+}
+ 
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import "bootstrap/dist/css/bootstrap.css"
-import Counter from "./components/counter"
 import "./static/css/index.css"
+import App from "./App"
 
-ReactDOM.render(<Counter />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
